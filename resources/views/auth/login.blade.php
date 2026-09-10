d1e137b665ce83b4caf230ff7b2d3f17755f4538
Farhan Robbani <farhan@example.com>
2026-09-10 13:06:45 +0700

    fix: add type=submit to form buttons for proper form submission

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button type="submit" class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -14,7 +14,7 @@
             @csrf
 
             <div>
-                <x-primary-button>
+                <x-primary-button type="submit">
                     {{ __('Resend Verification Email') }}
                 </x-primary-button>
             </div>
